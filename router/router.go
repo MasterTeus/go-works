717197a3b92376a58703a56f87788b5d5fff8e00
Mateus Cazuza <mateus.cazuza@hotmail.com>
2024-04-28 13:58:12 -0400

    feat(routes): create all routes the /opening

--- router/router.go
+++ router/router.go
@@ -3,13 +3,12 @@ package router
 import "github.com/gin-gonic/gin"
 
 func Initalize() {
-	// initicializa o Router usando as configs padrão do Gin
+	// Initialize Router
 	router := gin.Default()
-	// Definindo uma rota get
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	router.Run(":8080") // listen and serve on 0.0.0.0:8080
+
+	// Initialize Routes GET/POST/DELET...
+	initializeRoute(router)
+
+	// Run the server
+	router.Run(":8080")
 }
