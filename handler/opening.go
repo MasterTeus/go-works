bc197d90914cf8f62b11cd476d7d608394be9f1b
Mateus Cazuza <mateus.cazuza@hotmail.com>
2024-04-30 18:50:58 -0400

    feat(handler) add createOpening request

--- handler/opening.go
+++ handler/opening.go
@@ -3,29 +3,57 @@ package handler
 import (
 	"net/http"
 
+	"github.com/MasterTeus/go-works.git/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "POST Opening",
-	})
+	request := CreateOpeningRequest{}
+
+	ctx.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.Errorf("validation error: %v", err.Error())
+		sendRequestError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	oppening := schemas.Opening{
+		Role:     request.Role,
+		Company:  request.Company,
+		Link:     request.Link,
+		Location: request.Location,
+		Remote:   *request.Remote,
+		Salary:   request.Salary,
+	}
+
+	if err := db.Create(&oppening).Error; err != nil {
+		logger.Errorf("error creating opening: %v", err.Error())
+		sendRequestError(ctx, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	sendRequestSuccess(ctx, oppening)
 }
+
 func ShowOpeningHandler(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{
 		"msg": "GET Opening",
 	})
 }
+
 func ListOpeningsHandler(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{
 		"msg": "GET All Opening",
 	})
 }
+
 func UpdateOpeningHandler(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{
 		"msg": "PUT Opening",
 	})
 }
+
 func DeleteOpeningHandler(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{
 		"msg": "DELETE Opening",
--- schemas/schemas.go
+++ schemas/schemas.go
@@ -1,6 +1,8 @@
 package schemas
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
@@ -12,4 +14,17 @@ type Opening struct {
 	Link     string
 	Remote   bool
 	Salary   int64
+}
+
+type OpeningResponse struct {
+	ID        uint      `json:"id"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	DeletedAt time.Time `json:"deletedAt,omitempty"`
+	Role      string    `json:"role"`
+	Company   string    `json:"company"`
+	Location  string    `json:"location"`
+	Link      string    `json:"link"`
+	Remote    bool      `json:"remote"`
+	Salary    int64     `json:"salary"`
 }
